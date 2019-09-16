4790270e6e8797a1a0ddfd01c2974ff516818ff3
lpengshi <laips90@gmail.com>
2019-09-16 21:55:04 +0800

    clean up app.module

--- bgreview-client/src/app/app.module.ts
+++ bgreview-client/src/app/app.module.ts
@@ -1,70 +1,30 @@
-<<<<<<< HEAD
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './material.module';
-import {MatPaginatorModule} from '@angular/material/paginator';
-
-import { AppComponent } from './app.component';
-import { BoardgamesComponent } from './components/boardgames.component';
-import { ApprouteModule } from './approute.module';
-import { BoardgameService } from './boardgame.service';
-import { CategoriesComponent } from './components/categories.component';
-||||||| merged common ancestors
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { MaterialModule } from './material.module';
-
-import { AppComponent } from './app.component';
-import { BoardgamesComponent } from './components/boardgames.component';
-import { ApprouteModule } from './approute.module';
-import { BoardgameService } from './boardgame.service';
-import { CategoriesComponent } from './components/categories.component';
-=======
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { MaterialModule } from "./material.module";
->>>>>>> 9904efae0c39338f3a80260a282d4201528da80b
-
-import { AppComponent } from "./app.component";
-import { BoardgamesComponent } from "./components/boardgames.component";
-import { ApprouteModule } from "./approute.module";
-import { BoardgameService } from "./boardgame.service";
-import { CategoriesComponent } from "./components/categories.component";
-import { CommentsComponent } from "./components/comments.component";
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { AppComponent } from './app.component';
+import { BoardgamesComponent } from './components/boardgames.component';
+import { CategoriesComponent } from './components/categories.component';
+import { CommentsComponent } from "./components/comments.component";
 import { GameDetailComponent } from './components/game-detail.component';
+
+import { ApprouteModule } from './approute.module';
+import { BoardgameService } from './boardgame.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BoardgamesComponent,
-    CategoriesComponent,
-    CommentsComponent,
+    AppComponent, BoardgamesComponent,
+    CategoriesComponent, CommentsComponent,
     GameDetailComponent
   ],
   imports: [
-<<<<<<< HEAD
     BrowserModule,  BrowserAnimationsModule,
     HttpClientModule, MaterialModule, ApprouteModule,
-    MatPaginatorModule,
-||||||| merged common ancestors
-    BrowserModule,  BrowserAnimationsModule,
-    HttpClientModule, MaterialModule, ApprouteModule
-=======
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    MaterialModule,
-    ApprouteModule,
-    FormsModule,
-    ReactiveFormsModule
->>>>>>> 9904efae0c39338f3a80260a282d4201528da80b
+    MatPaginatorModule, FormsModule, ReactiveFormsModule
   ],
   providers: [BoardgameService],
   bootstrap: [AppComponent]
